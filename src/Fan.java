3208b643ee429c26c9ab3f949562ad33eb4d7ec8
anthonyassaf <62218418+anthonyassaf@users.noreply.github.com>
2020-12-11 19:42:38 +0200

    add fan colleague class

---
+++ src/Fan.java
@@ -0,0 +1,37 @@
+
+/**
+ *
+ * @author anthony
+ */
+public class Fan implements HomeComponent {
+    private int level;
+    private SmartHomeMediator smartHomeMediator;
+
+    @Override
+    public void on() {
+        System.out.println("Turning on the fan");
+        level = 1;
+        smartHomeMediator.changed(this);
+    }
+
+    @Override
+    public void off() {
+        System.out.println("Turning off the fan");
+        level = 0;
+        smartHomeMediator.changed(this);
+    }
+
+    @Override
+    public boolean isOn() {
+        return this.level != 0;
+    }
+
+    public void adjustLevel(int level) {
+        this.level = level;
+    }
+
+    public void setSmartHomeMediator(SmartHomeMediator smartHomeMediator) {
+        this.smartHomeMediator = smartHomeMediator;
+    }
+    
+}
